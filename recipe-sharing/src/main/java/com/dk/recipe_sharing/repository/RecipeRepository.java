5a860436e85026210964f55b472bf06a8a1bdf25
DHINESH05G <73932446+DHINESH05G@users.noreply.github.com>
2024-08-07 17:30:00 +0530

    Delete recipe-sharing directory

--- recipe-sharing/src/main/java/com/dk/recipe_sharing/repository/RecipeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dk.recipe_sharing.repository;
-
-import com.dk.recipe_sharing.model.Recipe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecipeRepository extends JpaRepository<Recipe,Long> {
-}
--- recipe-sharing/src/main/java/com/dk/recipe_sharing/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.dk.recipe_sharing.service;
-
-import com.dk.recipe_sharing.model.User;
-
-public interface UserService {
-
-    public User findUserById(Long userId)throws Exception;
-
-}
