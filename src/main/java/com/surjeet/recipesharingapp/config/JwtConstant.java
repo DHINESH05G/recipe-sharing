5c8fda476484dda7a962050e8f40d7ec8de713e3
DHINESH05G <73932446+DHINESH05G@users.noreply.github.com>
2024-08-28 17:14:14 +0530

    Delete src/main/java/com/surjeet/recipesharingapp directory

--- src/main/java/com/surjeet/recipesharingapp/config/JwtConstant.java
+++
@@ -1,9 +0,0 @@
-package com.surjeet.recipesharingapp.config;
-
-public class JwtConstant {
-    public static final String JWT_SECTRET="asuhiuadsghiuhdukhakjddsffkdsmlf";
-    public static final String JWT_HEADER="Authorization";
-
-
-
-}
